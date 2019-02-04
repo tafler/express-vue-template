2f72029fc1f693e43b0af7a8546d41fb0864ead4
Artem Tafincev <taffik@yandex.ru>
2019-02-04 23:33:07 +0300

    then catch to test

--- test/controller.api.test.js
+++ test/controller.api.test.js
@@ -1,11 +1,14 @@
 describe('api test', () => {
   it('should return 200 + text', (done) => {
     request.get('/api/getPost')
-    .end((err, res) => {
+    .then((res) => {
       expect(res).to.have.status(200);
       expect(res.text).to.be.a('string');
       expect(res.text.length).to.not.be.equal(0);
       done();
+    })
+    .catch(err => {
+      done(err)
     })
   });
 })
--- test/controller.main.test.js
+++ test/controller.main.test.js
@@ -1,11 +1,14 @@
 describe('test api main page', () => {
   it('we wait homepage', (done) => {
     request.get('/')
-    .end((err, res) => {
+    .then((res) => {
       expect(res).to.have.status(200);
       // expect(res.text).to.be.a('string');
       // expect(res.text.length).to.not.be.equal(0);
       done();
+    })
+    .catch(err => {      
+      done(err);
     })
   })
 })
